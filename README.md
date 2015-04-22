0d96fa74d643ede9db1d669e0e3e845939910e17
Malte Burkhardt <info@bitsundpixel.de>
2015-04-22 22:48:24 +0200

    quick n dirty getting to work

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # fiware-orion-probe
+
 A mini explorer for entities stored in FiWare Orion Context Explorer
+
+# Installation
+
+    npm install
+    
+Copy /config/config.sample.js to /config/config.js. and adjust settings. 
+    
+# Usage
+
+    node bin/www
+    
+Open http://localhost:3000 (if not specified otherwise in ENV)
+
+    '/' - list all entities in instance of context broker
+    '/entity/<ID> - query specified entity
+    '/type/<TYPE> - query all entities with specified type
---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../config/config.js');
+
+var orion = require('fiware-orion-mintaka');
+orion.configure(config.ORION);
+
+/* GET listing. */
+router.get('/', function(req, res, next) {
+  orion.requestEntities().then(
+          function(success){
+            res.send(success);
+          },
+          function(error){
+            res.send(error);
+          }
+  );
+});
+
+// query entity with attributes
+router.get('/entity/:entity',function(req, res, next){
+  var e = req.params.entity;
+  orion.queryAttributes(e).then(
+          function(success){
+            res.send(success);
+          },
+          function(error){
+            res.send(error);
+          }
+  );
+});
+
+router.get('/type/:type', function(req, res, next){
+  var t = req.params.type;
+  orion.queryEntitiesByType(t).then(
+          function(success){
+            res.send(success);
+          },
+          function(error){
+            res.send(error);
+          }
+  );
+});
+
+module.exports = router;
